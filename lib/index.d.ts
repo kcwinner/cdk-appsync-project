1e21903d1c3798279cf8326601866584b2902b7d
Ken Winner <ken@getvoxi.com>
2021-02-24 12:26:38 -0500

    forcibly add lib

---
+++ lib/index.d.ts
@@ -0,0 +1,25 @@
+import { AwsCdkTypeScriptApp, AwsCdkTypeScriptAppOptions } from 'projen';
+/**
+ * @experimental
+ */
+export interface AwsCdkAppSyncAppOptions extends AwsCdkTypeScriptAppOptions {
+    /**
+     * (experimental) cdk-appsync-transformer version to use.
+     *
+     * @default "1.77.9"
+     * @experimental
+     */
+    readonly transformerVersion: string;
+}
+/**
+ * (experimental) AWS CDK AppSync Transformer App in TypeScript.
+ *
+ * @experimental
+ * @pjid awscdk-appsync-app-ts
+ */
+export declare class AwsCdkAppSyncApp extends AwsCdkTypeScriptApp {
+    /**
+     * @experimental
+     */
+    constructor(options: AwsCdkAppSyncAppOptions);
+}
